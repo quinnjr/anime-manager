4a6e8c84ee123c8e78e468cafea9acd0826040e1
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2026-09-12 20:34:29 -0400

    feat: dlna browse tree over live db reads

--- src-tauri/src/dlna.rs
+++ src-tauri/src/dlna.rs
@@ -35,6 +35,54 @@ pub fn didl_for_episode(show_title: &str, season_no: u32, ep: &crate::models::Ep
     s
 }
 
+pub fn browse(db: &crate::db::Db, object_id: &str) -> crate::error::Result<String> {
+    use crate::models::ShowSort;
+    let (kind, id) = decode_id(object_id).unwrap_or(("root".into(), 0));
+    match kind.as_str() {
+        "root" => {
+            let s = String::from("<container id=\"shows:0\" parentID=\"root:0\" restricted=\"1\"><dc:title>Shows</dc:title></container>");
+            let _ = id;
+            Ok(s)
+        }
+        "shows" => {
+            let mut s = String::new();
+            for c in db.list_shows("", ShowSort::Title)? {
+                s.push_str(&format!("<container id=\"{}\" parentID=\"shows:0\" restricted=\"1\"><dc:title>{}</dc:title></container>",
+                    xml_escape(&encode_id("show", c.id)), xml_escape(&c.display_title)));
+            }
+            Ok(s)
+        }
+        "show" => {
+            let d = db.get_show(id)?;
+            let mut s = String::new();
+            for se in &d.seasons {
+                let label = se.title.clone().unwrap_or_else(|| format!("Season {}", se.number));
+                s.push_str(&format!("<container id=\"{}\" parentID=\"{}\" restricted=\"1\"><dc:title>{}</dc:title></container>",
+                    xml_escape(&encode_id("season", se.id)), xml_escape(object_id), xml_escape(&label)));
+            }
+            Ok(s)
+        }
+        "season" => {
+            // find parent show by scanning shows (small N in tests; paginate later if needed)
+            let mut s = String::new();
+            for c in db.list_shows("", ShowSort::Title)? {
+                let d = db.get_show(c.id)?;
+                for se in &d.seasons {
+                    if se.id == id {
+                        for ep in &se.episodes {
+                            if ep.status == crate::models::EpisodeStatus::Missing { continue; }
+                            let u = format!("/media/{}", encode_id("episode", ep.id));
+                            s.push_str(&didl_for_episode(&d.display_title, se.number, ep, &[u]));
+                        }
+                    }
+                }
+            }
+            Ok(s)
+        }
+        _ => Ok(String::new()),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -44,5 +92,28 @@ mod tests {
         assert_eq!(crate::dlna::decode_id(&s), Some(("episode".into(), 42)));
         assert_eq!(crate::dlna::decode_id("../etc"), None);
         assert_eq!(crate::dlna::xml_escape("<a>&\"'"), "&lt;a&gt;&amp;&quot;&apos;");
+    }
+
+    #[test]
+    fn browse_root_lists_shows_and_hides_missing() {
+        use std::path::PathBuf;
+        let db = crate::db::Db::open_memory().unwrap();
+        let p = crate::parser::ParsedName { title: "Browse Tree".into(), season: 1, episode: 1, release_group: None, resolution: None, crc: None };
+        let f = crate::scanner::RawFile { path: PathBuf::from("/lib/Browse Tree/01.mkv"), size: 1, mtime: 1, stem: "".into(), dirs: vec![] };
+        db.upsert_episode(&p, &f).unwrap();
+        let root = crate::dlna::browse(&db, "root:0").unwrap();
+        assert!(root.contains("shows:0"));
+        let shows = crate::dlna::browse(&db, "shows:0").unwrap();
+        assert!(shows.contains("Browse Tree"));
+        let show_id = db.list_shows("", crate::models::ShowSort::Title).unwrap()[0].id;
+        let show_out = crate::dlna::browse(&db, &crate::dlna::encode_id("show", show_id)).unwrap();
+        assert!(show_out.contains("Season 1"));
+        let season_id = db.get_show(show_id).unwrap().seasons[0].id;
+        let season_out = crate::dlna::browse(&db, &crate::dlna::encode_id("season", season_id)).unwrap();
+        assert!(season_out.contains("S01E01"));
+        let ep_id = db.get_show(show_id).unwrap().seasons[0].episodes[0].id;
+        db.set_status(ep_id, crate::models::EpisodeStatus::Missing).unwrap();
+        let hidden = crate::dlna::browse(&db, &crate::dlna::encode_id("season", season_id)).unwrap();
+        assert!(!hidden.contains("S01E01"));
     }
 }
